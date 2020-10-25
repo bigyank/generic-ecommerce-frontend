09f0dbc18cd2eb9fc7326974ddcd73fc07856267
bigyank <this.is.blueorange@gmail.com>
2020-10-25 14:49:25 +0545

    add react-router on nav and products

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import HomeScreen from './screen/HomeScreen';
+import ProductScreen from './screen/ProductScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" exact>
+            <HomeScreen />
+          </Route>
+          <Route path="/product/:id">
+            <ProductScreen />
+          </Route>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -9,12 +9,15 @@ const Rating = ({ rating, numReviews }) => {
   return (
     <div>
       <span>
+        {/* number of full stars */}
         {[...Array(Math.floor(rating))].map((_, index) => (
           <i key={index} className="fas fa-star"></i>
         ))}
 
+        {/* add halfstar if needed */}
         {!isInt(rating) && <i className="fas fa-star-half-alt"></i>}
 
+        {/* remaining empty star out of 5 */}
         {[...Array(5 - Math.ceil(rating))].map((_, index) => (
           <i key={index} className="far fa-star"></i>
         ))}
